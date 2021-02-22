2be1f5216e3718df96e7e32dd3190072951be381
Nikita Kurgan <kurgan123456@gmail.com>
2021-02-22 15:44:40 +0300

    added obstacle class

---
+++ app/src/main/java/com/example/flyaircraft/Objects/Obstacle.java
@@ -0,0 +1,20 @@
+package com.example.flyaircraft.Objects;
+
+import java.util.TimerTask;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class Obstacle {
+    private float x;
+    private Paint paint = new Paint();
+
+
+
+
+    void onDraw(Canvas canvas){
+        canvas.drawRect();
+    }
+
+
+}
